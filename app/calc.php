ec3f6ed57b2902231f473ceda6da49fe7389448c
Basia Turniak <144422201+Qasiers@users.noreply.github.com>
2023-09-22 11:19:28 +0200

    Update calc.php

--- app/calc.php
+++ app/calc.php
@@ -1,11 +1,12 @@
 <?php
 require_once dirname(__FILE__)."/../config.php";
 $sum = $_REQUEST["sum"];
-$proc = 0.02;
+$proc = $_REQUEST["proc"];
 $time = $_REQUEST["time"];
-if (!empty($sum) && !empty($time)) {
-    if (is_numeric($sum) && is_numeric($time)) {
-        if ($sum <= 200000 && $time <= 120) {
+if (!empty($sum) && !empty($time) && !empty($proc)) {
+    if (is_numeric($sum) && is_numeric($time) && is_numeric($proc)) {
+        if ($sum <= 200000 && $time <= 120 && $proc >= 1) {
+            $proc /= 100;
             $ins = ($proc/12)*$time;
             $whole = $sum+($sum*$proc);
             $month = $whole/$time;
@@ -13,8 +14,18 @@ if (!empty($sum) && !empty($time)) {
             $whole = number_format($whole,0,","," ");
             $msg = "Kwota pojedynczej raty: $month<br /> Kwota do spłaty: $whole<br />";
             $err = "'no_err'";
+            $proc *= 100;
+        } else if ($proc < 1 && $proc > 0) {
+            $ins = ($proc/12)*$time;
+            $whole = $sum+($sum*$proc);
+            $month = $whole/$time;
+            $month = number_format($month,0,","," ");
+            $whole = number_format($whole,0,","," ");
+            $msg = "Kwota pojedynczej raty: $month<br /> Kwota do spłaty: $whole<br />";
+            $err = "'no_err'";
+            $proc *= 100;
         } else {
-            $msg = "Podano za dużą wartość.";
+            $msg = "Podano nieprawidłową wartość.";
             $err = "'err'";
         }
     } else {
