655524626bc61613c461ec1e2399d39f257bad42
Basia Turniak <144422201+Qasiers@users.noreply.github.com>
2024-01-10 12:53:06 +0100

    Update config.php

--- Kalkulator_klasy/config.php
+++ Kalkulator_klasy/config.php
@@ -5,7 +5,7 @@ $conf = new config();
 
 $conf->server_name = 'localhost:80';
 $conf->server_url = 'http://'.$conf->server_name;
-$conf->app_root = '/klasa_V/2023.12.01/zadanie';
+$conf->app_root = '/Kalkulator_klasy';
 $conf->app_url = $conf->server_url.$conf->app_root;
 $conf->root_path = dirname(__FILE__);
 ?>
